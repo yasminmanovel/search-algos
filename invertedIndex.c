5e11e92d497af2a19e9354dc380a6945cd5d3a03
yasminmanovel <yasminmanovel@outlook.com>
2018-10-10 13:24:07 +1100

    more comments

---
+++ invertedIndex.c
@@ -0,0 +1,9 @@
+/*
+Need to  call GetCollection() to get List_of_Urls
+Functions in this file:
+	GetInvertedList(List_of_Urls)
+		returns InvertedIndex invertedldx
+Output invertedldx to "invertedIndex.txt"
+
+
+*/
