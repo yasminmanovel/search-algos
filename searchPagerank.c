daa82d0dd526813411d5217a3bad7f0972c37e19
yasminmanovel <yasminmanovel@outlook.com>
2018-10-10 13:28:30 +1100

    more comments

---
+++ searchPagerank.c
@@ -0,0 +1,10 @@
+/*
+Get query words from arguments
+Need these functions:
+	findMatchedUrls("invertedIndex.txt", queryWords)
+		return matched_Url_list
+	findPagerank("pagerankList.txt", matched_Url_list)
+		return matched_Urls_with_PR
+Output order urls on stdout
+
+*/
