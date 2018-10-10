8a8dc313f02952074f9f60d39ba4b9339fee9c9b
yasminmanovel <yasminmanovel@outlook.com>
2018-10-10 13:14:56 +1100

    added files and info about task

---
+++ readData.c
@@ -0,0 +1,18 @@
+/*
+Functions we need to have:
+	getCollection()
+		returns List_of_Urls
+		Create a set (list) of urls to process by reading data from file "collection.txt"
+	getGraph(List_of_Urls)
+		Returns Graph g
+		Create empty graph (use graph ADT in say graph.h and graph.c)
+		For each url in the above list
+			- Read <url>.txt file, and update graph by adding a node and outgoing links
+	GetInvertedList(List_of_Urls)
+		Create empty inverted list (use say List of lists, BST where values are lists, etc)
+		For each url in List_of_Urls
+			- Read <url<.txt file, and update inverted index	
+
+
+
+*/
