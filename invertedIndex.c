ae607bc164984364857e95bfb286b0bc042aac2d
Selina Chua <selina.a.chua@gmail.com>
2018-10-13 17:41:52 +1100

    added BSTree and bst files

--- invertedIndex.c
+++ invertedIndex.c
@@ -16,12 +16,5 @@
  * 
  * Output -> invertedIdx to "invertedIndex.txt"
  */
-/*
-Need to  call GetCollection() to get List_of_Urls
-Functions in this file:
-	GetInvertedList(List_of_Urls)
-		returns InvertedIndex invertedldx
-Output invertedldx to "invertedIndex.txt"
 
 
-*/
