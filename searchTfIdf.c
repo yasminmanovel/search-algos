64fa305d0eaea3fa6c3b7d9cfc97349f93c57a08
Selina Chua <selina.a.chua@gmail.com>
2018-10-18 00:59:34 +1100

    Started on tfidf. Have not done much.

--- searchTfIdf.c
+++ searchTfIdf.c
@@ -8,7 +8,40 @@
  * Content based search engine that uses tf-idf values.
  * Rank pages based on summation of tf-idf values for all query terms. 
  * 
- *
- * 
- * 
+ * tf = term frequency = (# of appearance) / (total num of words in doc)
+ * idf = accounts for ratio of documents that include word
+ *     = log( (total docs)/(# of docs word appears in) )
+ * tf-idf = tf * idf
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "set.h"
+#include "readData.h"
+
+int main(int argc, char **argv) 
+{
+    float tf, idf, tfIdf;
+    // For each search word in command line argument.
+    int i;
+    for(i = 0; i < argc; i++) {
+        getURLs(argv[i+1]); // Gets the URLs for search word.
+    }
+
+    return 0;
+}
+
+/* Gets the URLs that contain word. */
+Set getURLs(char *word) {
+    FILE *invIndex = fopen("invertedIndex.txt", "r");
+    if (!invIndex) { perror("fopen failed"); exit(EXIT_FAILURE); }
+    // Finds the wanted word.
+
+
+}
+
+/* Calculates how frequently a term appears in a url. */
+float tf(FILE *URLFile, char *word) 
+{
+    
+}
