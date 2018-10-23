2faa89f7d2f041168847218af9cf8d1ca3c1ae32
Selina Chua <selina.a.chua@gmail.com>
2018-10-24 09:35:07 +1100

    deleted functions in BSTree

--- BSTree.h
+++ BSTree.h
@@ -1,4 +1,8 @@
-// BSTree.h ... interface to binary search tree ADT
+/* BSTree.h ... interface to binary search tree ADT
+ * Taken from COMP2521 lab10.
+ * Modified by Selina and Yasmin for COMP2521 ass2.
+ * Group name: duckduckgo
+ */
 
 #ifndef BSTREE_H
 #define BSTREE_H
@@ -14,30 +18,11 @@ typedef struct listNode {
 BSTree newBSTree();
 // free memory associated with BSTree
 void dropBSTree(BSTree);
-// display a BSTree
-// void showBSTree(BSTree);
 // display BSTree root node
 void showBSTreeNode(FILE *, BSTree);
-
 // print values in infix order
 void BSTreeInfix(FILE *, BSTree);
-// print values in prefix order
-void BSTreePrefix(BSTree);
-// print values in postfix order
-void BSTreePostfix(BSTree);
-// print values in level-order
-// void BSTreeLevelOrder(BSTree);
-
-// count #nodes in BSTree
-int BSTreeNumNodes(BSTree);
-// count #leaves in BSTree
-int BSTreeNumLeaves(BSTree);
-
 // insert a new value into a BSTree
 BSTree BSTreeInsert(BSTree, char *, char *);
-// check whether a value is in a BSTree
-int BSTreeFind(BSTree, char *);
-// delete a value from a BSTree
-BSTree BSTreeDelete(BSTree, char *);
 
 #endif
