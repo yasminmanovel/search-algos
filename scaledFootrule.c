17adb178c5f45b6a0dae65cd322fdff374191e74
Selina Chua <selina.a.chua@gmail.com>
2018-10-23 11:58:31 +1100

    can get information from given arguments

--- scaledFootrule.c
+++ scaledFootrule.c
@@ -20,15 +20,17 @@
 #define END_TAG_LEN     14
 #define CHAR_LEN        1
 
-typedef rankFile *rankFP;
-
 typedef struct _rankFile {
     char  *fileName;
-    int    fileSize;
-    char **urls;
+    char **URLs;
+    int    nURLs;
 } rankFile;
 
+typedef rankFile *rankFP;
+
+rankFP newRankFile();
 void insertURLs(Set unionURL, char *fileName);
+void showRankFile(rankFP file);
 
 int main(int argc, char **argv) 
 {
@@ -38,21 +40,64 @@ int main(int argc, char **argv)
     // Get union of all URLs.
     Set unionURL = newSet();
     // Creates a 2D array of [file][url].
-    rankFile *files = malloc(nFiles * sizeof(rankFP));
+    rankFP *files = calloc(nFiles, sizeof(rankFP));
     for (i = 0; i < nFiles; i++) {
-        URLs[i] = insertURLs(unionURL, argv[i+1]);
+        files[i] = newRankFile(argv[i+1]);
+        insertURLs(unionURL, argv[i+1]);
     }
-
+    return 0;
 }
 
 /* Calculates the scaled foot rule distance. */
-double calcSFRDist()
-{
+// double calcSFRDist()
+// {
 
+// }
+
+/* Creates a new rankFile struct. */
+rankFP newRankFile(char *file)
+{
+    char line[URL_LENGTH] = {0};
+
+    rankFile *new = calloc(1, sizeof(rankFile));
+    new->fileName = strdup(file);
+    new->nURLs = 0;
+    
+    FILE *fp = fopen(file, "r");
+    if (!fp) { perror("fopen failed"); exit(EXIT_FAILURE); }
+    // Counts number of URLs in file.
+    while (fgets(line, URL_LENGTH, fp) != NULL)
+        new->nURLs++;
+    // Goes back to beginning of the file.
+    rewind(fp);
+
+    // Creates a string of URLs separated by spaces.
+    char *URLs = calloc(new->nURLs, URL_LENGTH);
+    while (fgets(line, URL_LENGTH, fp) != NULL) {
+        char *word = normalise(line);
+        strcat(URLs, " ");
+        strcat(URLs, word);
+        free(word);
+    }
+    new->URLs = tokenise(URLs, " ");
+    fclose(fp);
+
+    return new;
+}
+
+void showRankFile(rankFP file) 
+{
+    printf("NAME %s\n", file->fileName);
+    printf("\tnURLS %d\n", file->nURLs);
+    printf("\tURLS:");
+    int i;
+    for (i = 0; file->URLs[i] != NULL; i++)
+        printf(" %s", file->URLs[i]);
+    printf("\n");
 }
 
 /* Gets URLs in file and place it into set. */
-char **insertURLs(Set unionURL, char *fileName)
+void insertURLs(Set unionURL, char *fileName)
 {
     // Opens the file.
     FILE *fp = fopen(fileName, "r");
